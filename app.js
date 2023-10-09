af71c8710172b0dbde67fe8bbc879682f9e908a9
Elizabeth Hunt <elizabeth.hunt@simponic.xyz>
2023-10-09 14:06:09 -0600

    check if fl0 is running a persistent container

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
+app.get("*", (req, res) => {
+  res.redirect("/");
+});
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+  console.log(`App listening on port ${port}`);
+});
+
+setInterval(() => fetch("https://simponic.xyz"), 10_000);
